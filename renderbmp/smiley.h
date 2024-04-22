5b3879d08b7165a47246f72b397a28866a566498
Martijn Boekhorst <martijn@boekhorst.net>
2024-04-22 18:59:53 +0200

    A bigger smiley in various stages of conversion ..
    .. odg -> svg -> png -> .c
    
    (odg = libreoffice draw)

---
+++ renderbmp/smiley.h
@@ -0,0 +1,28 @@
+/* Copyright 2024 Kinglet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License", builtin_not_implemented_runtime, builtin_not_implemented_eval);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef SMILEY_H
+#define SMILEY_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern unsigned char g_smiley_256x256_luminance_data_[];
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* SMILEY_H */
