063588aa1d6b05ba388e45310ff870baf094d6ff
Martijn Boekhorst <martijn@boekhorst.net>
2024-06-18 11:28:59 +0200

    Fix: Texture sampler, mask T using the T mask and S using the S mask ..
    .. masked a T (typically vertical axis on texture) with an S-mask, which
    caused issues on wrapping texture modes with non-square textures.

--- src/sampler_2d.c
+++ src/sampler_2d.c
@@ -503,7 +503,7 @@ static void texture2D(float *prgba, struct sampler_2d *s2d, float s, float t, fl
 
       /* Flip Y axis as we do this */
       uint32_t tex_t_fp8 = ((uint32_t)((1.f - tm) * s2dm->height_ * 256.f)) - 128;
-      uint32_t tex_t0 = (tex_t_fp8 >> 8) & s2dm->repeat_mask_s_;
+      uint32_t tex_t0 = (tex_t_fp8 >> 8) & s2dm->repeat_mask_t_;
       uint32_t tex_t1 = (tex_t0 + 1) & s2dm->repeat_mask_t_;
       uint32_t tex_t1_fract = tex_t_fp8 & 255;
       uint32_t tex_t0_fract = 256 - tex_t1_fract;
